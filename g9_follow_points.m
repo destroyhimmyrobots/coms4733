2464c92c5be0ce2de9ef665ee180099eb1d04e73
Marc J. Szalkiewicz <marc.szalkiewicz@gmail.com>
2013-11-20 20:02:54 -0500

    Fucks up on one point but gets to the goal

--- g9_follow_points.m
+++ g9_follow_points.m
@@ -43,21 +43,28 @@ function pos = go_to_point(serPort, pos, next)
     % Create endpoint of vector of distance d straight ahead.
     % Create a vector from the current position to the new position
 
-    % !!!!!!!!!!! FIX ME.
-    x_ahead = next(1) / cos(pos(3)); 
-    y_ahead = x_ahead * sin(pos(3));
-    
-    v1 = [x_ahead - pos(1), y_ahead - pos(2)];
-    v2 = [next(1) - pos(1), next(2) - pos(2)]; % Technically d should be ||v_2||
-    
+    % dtmp = arbitrary distance in front current bearing
+    v1 = [10*cos(pos(3)), 10*sin(pos(3))];
+    v2 = [next(1) - pos(1), next(2) - pos(2)];
+
     % Get angle between those vectors
     t = vec_angle(v1, v2);
+    
     % Turn and advance
     pos = advance(serPort, t, d, pos);
 end
 
 function pos = advance(serPort, t, d, pos)
-    avel     = 0.14;
+    if(t > 10)
+        avel = 0.16;
+    elseif(t > 1)
+        avel = 0.12;
+    elseif(t > 0.1)
+        avel = 0.08;
+    else
+        avel = 0.026;
+    end
+    
     vel      = 0.4;
     tick     = 0.15;
     ticks    = 0;
@@ -69,12 +76,15 @@ function pos = advance(serPort, t, d, pos)
     pause(tick);
     
     % Turn
-    turnAngle(serPort, avel, t);
-    old_ang = pos(3);
-    pos     = update_pos(0, AngleSensorRoomba(serPort), pos);
-    turned  = pos(3) - old_ang;
-    fprintf('Turned %0.5g of %0.5g desired degrees.\n', turned, t);
-    pause(tick);
+    if(abs(t) > 1e-4)
+        turnAngle(serPort, avel, convert2deg(t));
+        turned  = AngleSensorRoomba(serPort);
+        pos     = update_pos(0, turned, pos);
+        fprintf('Turned %0.5g of %0.5g desired rads.\n', turned, t);
+        pause(tick);
+    else
+        fprintf('Chose not to turn. %0.5g is less than threshold\n', t);
+    end
     
     % Advance by distance d
     SetFwdVelAngVelCreate(serPort, vel, 0);
@@ -95,7 +105,6 @@ function pos = advance(serPort, t, d, pos)
     pause(tick);
     
     % Correct for any angular errors during turn & motion.
-    turned = pos(3) - old_ang;
     pos = correct(serPort, tick, d, d_sensor, avel, t, turned, pos);
 end
 
@@ -106,19 +115,21 @@ function pos = correct(serPort, tick, d, d_sensor, avel, t, t_real, pos)
     % Currently corrects by a calculated rather than odometrically measured
     % angle. FIX?
     a_overshot = t_real - t;
-    if(abs(a_overshot) > 1e-3)
+    if(abs(a_overshot) > 1e-6)
         epsilon(3) = a_overshot;
         
-        turnAngle(serPort, avel, -epsilon(3));
+        turnAngle(serPort, avel, convert2deg(-epsilon(3)));
         pause(tick);
         
-        true_turn = AngleSensorRoomba(serPort) * (180/pi);
+        true_turn = AngleSensorRoomba(serPort);
         pause(tick);
         
         % Assign new angle here to update x, y correctly.
         pos(3) = pos(3) - epsilon(3);
-        fprintf('Desired  ang. correction after move:\t %0.5g deg.\n', -a_overshot);
-        fprintf('Measured ang. correction after move:\t %0.5g deg.\n', true_turn);
+        fprintf('Desired  ang. correction after move:\t %0.5g rad.\n', -a_overshot);
+        fprintf('Measured ang. correction after move:\t %0.5g rad.\n', true_turn);
+    else
+        fprintf('Not correcting angle. Below threshold.');
     end
     
     pos = [pos(1) + cos(pos(3))*epsilon(1) ...
@@ -134,13 +145,13 @@ function t = vec_angle(v1, v2)
     n2 = norm(v2, 2);
     
     ct = dot(v1, v2) / (n1 .* n2);
-    t  = acos(ct) * (180/pi);
+    t  = acos(ct);
     
     fprintf('Angle between current & next bearing:\t%0.5g\n', t);
 end
 
 function pos = update_pos(d, rad, pos)
-    pos(3) = pos(3) + rad*(180/pi);    
+    pos(3) = wrap_to_pi(pos(3) + rad);
     pos(1) = pos(1) + cos(pos(3)) * d;
     pos(2) = pos(2) + sin(pos(3)) * d;
     
@@ -152,6 +163,19 @@ function d = dist(now, later)
     d = sqrt((later(1)-now(1)).^2 + (later(2)-now(2)).^2);
 end
 
+function rad = wrap_to_pi(rad)
+    if(rad < -2*pi)
+        while (rad < -2*pi)
+            rad = rad + 2*pi;
+        end
+    elseif(rad > 2*pi)
+        while (rad > 2*pi)
+            rad = rad - 2*pi;
+        end
+    else
+        rad = rad;
+    end
+end
 function xy = parse_points(filename)
     fid = fopen(filename, 'rt');
     xy_text  = textscan(fid, '[%f, %f]');
