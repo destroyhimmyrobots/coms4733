102bf1490b44114485c5bf1c3b9625128c022a1b
Enrique Cruz <eac2192@columbia.edu>
2013-12-09 19:57:22 -0500

    work on robot controlling

--- hw5_tester.m
+++ hw5_tester.m
@@ -1,12 +1,28 @@
 im = imread('highway.jpg');
+avel = 0.1;
+vel      = 0.4;
+tick     = 0.15;
 
-[x, y] = imclick(im)
+active = 1;
+[x, y] = imclick(im);
 x = round(x);
 y = round(y);
 binary = thresh(im, x, y);
-
 [cen_x, cen_y, size] = imstats(binary);
 
-binary(cen_y-6:cen_y+6, cen_x-6:cen_x+6) = 0;
-
+while(active)
+    binary = thresh(im, cen_x, cen_y);
+    [new_cen_x, new_cen_y, new_size] = imstats(binary);
+    %binary(cen_y-6:cen_y+6, cen_x-6:cen_x+6) = 0;
+    if (new_cen_x < cen_x - BUFFER)
+        turnAngle(serPort, avel, -5);
+    end
+    
+    if (new_cen_x > cen_x + BUFFER)
+        turnAngle(serPort, avel, 5);
+    end
+    SetFwdVelAngVelCreate(serPort, vel, 0);
+    pause(tick)
+end
+    
 imshow(binary);
